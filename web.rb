159c061d4d63e9232008e2da9a980bf5cb2f070e
Gojko Adzic <gojko@gojko.com>
2013-12-17 10:29:28 +0000

    check referrer

--- web.rb
+++ web.rb
@@ -16,7 +16,7 @@ get '/' do
   uri=URI.parse(params[:u])
   begin
     result = uri.read :content_length_proc => lambda{|length| fail_with "Image too large #{length}" if length>settings.max_size}
-    fail_with "Domain #{request.host} not supported" unless settings.allowed_hosts.include? request.host 
+    fail_with "Domain #{request.referrer} not supported" unless settings.allowed_hosts.include? URI.parse(request.referrer || '').host 
     fail_with "Content type #{result.content_type} not supported" unless result.content_type.start_with? 'image'
     halt 200, {'Access-Control-Allow-Origin' => settings.cors_header, 'Content-Type' => result.content_type}, result
   rescue Exception => e
